979cb84b7edb85f33e99f21bd8b57d9f59712cb4
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-07 19:52:41 +0530

    updated route

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
 //Init Middleware 
 app.use(express.json({ extended: false}));
 
-app.get('/', (req, res) => res.send("Welcome Jeevan Joti Dash"));
+app.get('/', (req, res) => res.send("Welcome to Applolos"));
 
 // Use Routes
 app.use('/api/doctors', doctors);
